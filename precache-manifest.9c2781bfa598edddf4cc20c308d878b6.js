51f7953592407fd9720a0bb55653637f47c19649
Herrison feres <herrisonf_nascimento@hotmail.com>
2020-08-08 21:30:17 -0300

    Updates

---
+++ precache-manifest.9c2781bfa598edddf4cc20c308d878b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8124ff82265e40dc51746d75ddaf9805",
+    "url": "/index.html"
+  },
+  {
+    "revision": "65e52500c78d2fac3cf3",
+    "url": "/static/js/2.9b6babb3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9b6babb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38aab3c6931b6cb9eb3b",
+    "url": "/static/js/main.ac88663b.chunk.js"
+  },
+  {
+    "revision": "8f2579ff55bd5aa1997a",
+    "url": "/static/js/runtime-main.87831706.js"
+  }
+]);
---
+++ static/js/main.ac88663b.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpportifolio=this.webpackJsonpportifolio||[]).push([[0],{3:function(e,t,n){e.exports=n(8)},8:function(e,t,n){"use strict";n.r(t);var o=n(0),r=n.n(o),a=n(2);var i=function(){return r.a.createElement("div",null,"Teste")};n.n(a).a.render(r.a.createElement(r.a.StrictMode,null,r.a.createElement(i,null)),document.getElementById("root"))}},[[3,1,2]]]);
+//# sourceMappingURL=main.ac88663b.chunk.js.map
