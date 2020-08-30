19820f5c09bae1921236a6274110f351f00a4e14
Herrison feres <herrisonf_nascimento@hotmail.com>
2020-08-30 09:41:53 -0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9c2781bfa598edddf4cc20c308d878b6.js"
+  "/precache-manifest.6c7678f96098866600640fa35f79bf40.js"
 );
 
 self.addEventListener('message', (event) => {
