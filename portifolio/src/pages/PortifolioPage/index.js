850c33ccd62505102506959cc0a7dd60973f5e5d
Herrison feres <herrisonf_nascimento@hotmail.com>
2020-09-17 16:45:18 -0300

    Ajuste temporário do slider

--- portifolio/src/pages/PortifolioPage/index.js
+++ portifolio/src/pages/PortifolioPage/index.js
@@ -11,20 +11,43 @@ import './style.scss';
 const PortifolioPage = () => {
     const settings = {
         dots: true,
-        lazyLoad: true,
+        fade: true,
         infinite: true,
-        speed: 500,
+        autoplay: true,
         slidesToShow: 1,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        autoplaySpeed: 3000,
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                initialSlide: 2
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+          ]
       };
     return( 
         <Container>
             <TextBoxSectionComponent text="Projetos"/>
-            <div style={{
-                width: "200px",
-                height: "200px",
-            }}
-          >
+            <div className="testeCenter">
               <Slider {...settings} >
                 {
                     projectsData.projects.map(project => {
