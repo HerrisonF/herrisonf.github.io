49659b83ad4733b75eb4146afae1af3a04aec52d
Herrison feres <herrisonf_nascimento@hotmail.com>
2021-01-15 07:00:31 -0300

    Trocando foto

--- portifolio/src/components/headerImageComponent/index.js
+++ portifolio/src/components/headerImageComponent/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Row, Col} from 'react-bootstrap';
-import foto from './../../assets/foto.jpeg';
+import foto from './../../assets/foto.png';
 import './style.scss';
 
 const HeaderImage = () => {
