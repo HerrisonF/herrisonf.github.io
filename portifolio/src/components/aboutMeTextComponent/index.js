2cd1826a4e6c3b4d621d73fdd999ee8029589113
Herrison feres <herrisonf_nascimento@hotmail.com>
2021-01-26 08:29:02 -0300

    Alteração texto

--- portifolio/src/components/aboutMeTextComponent/index.js
+++ portifolio/src/components/aboutMeTextComponent/index.js
@@ -6,13 +6,13 @@ const AboutMeText = () => {
     return (
         <Container className="format-aboutme">
             <Row>
-                <span className="text-green">Desenvolvedor Mobile e aspirante à tecnologias como Unity ou Unreal. Pretendo fazer a diferença por meio das minhas soluções criativas. </span>   
+                <span className="text-green">Desenvolvedor Mobile e aspirante à tecnologias como Unity ou IA. Pretendo fazer a diferença por meio das minhas soluções criativas. </span>   
             </Row>
             <Row>
                 <p>Minha experiência com desenvolvimento profissional iniciou-se em Delphi na área de automação fiscal e refatoração. Atualmente, minhas atenções se voltaram ao desenvolvimento Mobile por gostos pessoais e necessidades profissionais. </p>
                 <p>Alguns de meus conhecimentos: Desenvolvimento Back-end e Front-End; Programação MVC, MVP e MVVM; Utilização de práticas Clean Code; Conhecimento de Git/Flow, SQL, Noções de Padrões de Projeto, Flutter, Docker, Kubernetes, React, SCRUM, JIRA, produtos Oracle e AWS.</p>
                 <p>Alguns projetos em que participei: Automação de comércios(Delphi, Desktop), Aplicativo Meu Técnico(Claro, Mobile), Portal LGPD(Claro, React), Migração do regionalizador da OI(OI, Oracle Infrastructure Cloud), Integração CIP(CIP, Oracle Cloud) entre outros.</p>
-                <p>Apesar da experiência em outras áreas, tenho preferência e maior domínio na plataforma mobile. Minha perspectiva para o futuro é o aprimoramento do meu conhecimento em mobile. </p>
+                <p>Apesar da experiência em outras áreas, tenho preferência e maior domínio na plataforma mobile. Minha perspectiva para o futuro é o aprimoramento do meu conhecimento em mobile, IA e Jogos. </p>
             </Row>
         </Container>  
     );
