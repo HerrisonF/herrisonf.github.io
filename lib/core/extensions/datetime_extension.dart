a805a58bc222d9e6cb82a62e80ef4f5b6f58164b
Herrison Féres <herrisonf_nascimento@hotmail.com>
2025-09-06 14:27:44 -0300

    Äjuste

--- lib/core/extensions/datetime_extension.dart
+++
@@ -1,8 +0,0 @@
-extension DateTimeAgeExtension on DateTime {
-  /// Calcula a idade a partir da dateTime.now recebido.
-  int get howOldIam {
-    Duration difference = this.difference(DateTime.utc(1997,DateTime.june));
-    double born = difference.inDays / 365;
-    return born.toInt();
-  }
-}
